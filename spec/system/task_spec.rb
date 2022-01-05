143fa33b2de223138f67bf704a9934b8f04cdafa
minorusso <gumbomb1@gmail.com>
2022-01-06 00:35:42 +0900

    タスク一覧テスト追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,34 +2,42 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
     describe '新規作成機能' do
         context 'タスクを新規作成した場合' do
-            before do
-                Task.create!(title: 'タスク名', details:'詳細')
-            end
             it '作成したタスクが表示される' do
                 visit new_task_path
-                fill_in 'Title', with: 'タスク名'
-                fill_in 'Details', with: '詳細'
-                click_button 'Create Task'
-                expect(page).to have_content 'タスク名'
-                expect(page).to have_content '詳細'
+                fill_in 'タイトル', with: 'テストタイトル'
+                fill_in '詳細', with: 'テスト詳細'
+                click_button '登録する'
+                expect(page).to have_content 'テストタイトル'
+                expect(page).to have_content 'テスト詳細'
             end
         end
     end
     describe '一覧表示機能' do
         context '一覧画面に遷移した場合' do
             it '作成済みのタスク一覧が表示される' do
-                task = FactoryBot.create(:task, title: 'task')
+                FactoryBot.create(:task)
+                FactoryBot.create(:second_task)
                 visit tasks_path
-                expect(page).to have_content 'task'
+                expect(page).to have_content 'test_details'
+            end
+        end
+        context 'タスクが作成日時の降順に並んでいる場合' do
+            it '新しいタスクが一番上に表示される' do
+                FactoryBot.create(:task)
+                FactoryBot.create(:second_task)
+                visit tasks_path
+                task_list = all('.task_list')
+                expect(task_list[0]).to have_content 'test_title2'
+                expect(task_list[1]).to have_content 'test_title1'
             end
         end
     end
     describe '詳細表示機能' do
         context '任意のタスク詳細画面に遷移した場合' do
             it '該当タスクの内容が表示される' do
-                @task = FactoryBot.create(:task, title: 'task')
+                @task = FactoryBot.create(:task)
                 visit task_path(@task)
-                expect(page).to have_content 'task'
+                expect(page).to have_content 'test_details'
             end
         end
     end
