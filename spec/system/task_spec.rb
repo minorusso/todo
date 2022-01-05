c0a4f5eb47417529ea1021747861f6760fa7c722
minorusso <gumbomb1@gmail.com>
2022-01-06 00:48:29 +0900

    タスク一覧テスト修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+    let!(:task){FactoryBot.create(:task)}
+    let!(:second_task){FactoryBot.create(:second_task)}
+    before do 
+        visit tasks_path
+    end
     describe '新規作成機能' do
         context 'タスクを新規作成した場合' do
             it '作成したタスクが表示される' do
@@ -15,17 +20,11 @@ RSpec.describe 'タスク管理機能', type: :system do
     describe '一覧表示機能' do
         context '一覧画面に遷移した場合' do
             it '作成済みのタスク一覧が表示される' do
-                FactoryBot.create(:task)
-                FactoryBot.create(:second_task)
-                visit tasks_path
                 expect(page).to have_content 'test_details'
             end
         end
         context 'タスクが作成日時の降順に並んでいる場合' do
             it '新しいタスクが一番上に表示される' do
-                FactoryBot.create(:task)
-                FactoryBot.create(:second_task)
-                visit tasks_path
                 task_list = all('.task_list')
                 expect(task_list[0]).to have_content 'test_title2'
                 expect(task_list[1]).to have_content 'test_title1'
@@ -35,8 +34,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     describe '詳細表示機能' do
         context '任意のタスク詳細画面に遷移した場合' do
             it '該当タスクの内容が表示される' do
-                @task = FactoryBot.create(:task)
-                visit task_path(@task)
+                visit task_path(task)
                 expect(page).to have_content 'test_details'
             end
         end
