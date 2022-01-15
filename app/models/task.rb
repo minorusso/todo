d8f97058f09981849fa8a666ab4badac928a7a36
minorusso <gumbomb1@gmail.com>
2022-01-15 11:10:33 +0900

    アソシエーション構築と最初のシードデータ追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :user 
   validates :title, presence:true
   validates :details, presence:true
   validates :time_limit, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_005504) do
+ActiveRecord::Schema.define(version: 2022_01_15_015841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2022_01_15_005504) do
     t.datetime "time_limit"
     t.integer "completed"
     t.integer "priority"
+    t.bigint "user_id"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_01_15_005504) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
