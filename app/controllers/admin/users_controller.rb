812c22e86e0fdedf3e7814f9844e5e52be92575c
minorusso <gumbomb1@gmail.com>
2022-01-15 15:26:49 +0900

    Admin controller & view の追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,45 @@
+class Admin::UsersController < ApplicationController
+    # before_action :if_not_admin
+    skip_before_action :login_required
+    def index
+        @users = User.all
+    end
+    def new
+        @user = User.new
+    end
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            redirect_to admin_users_path, notice: 'ユーザーを登録しました!'
+        else
+            render :new 
+        end
+    end    
+    def edit
+        @user = User.find(params[:id])
+    end   
+    def update 
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to admin_users_path, notice: 'ユーザーを編集しました!'
+        else
+            render :edit 
+        end
+    end    
+    def show
+        @user = User.find(params[:id])
+    end
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to admin_users_path, notice:'ユーザーを削除しました', data: {confirm: '本当に削除しますか？'}
+    end
+
+    private
+    # def if_not_admin
+    #     redirect_to root_path unless current_user.admin?
+    # end
+    def user_params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation,:admin)
+    end
+end
