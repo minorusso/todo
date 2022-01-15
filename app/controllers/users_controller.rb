94f404d4970c87a84d09d604be2103960b995b25
minorusso <gumbomb1@gmail.com>
2022-01-15 11:47:30 +0900

    自分以外のマイページへのアクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user.id)
         else
             render :new
@@ -13,6 +14,9 @@ class UsersController < ApplicationController
     end
     def show
         @user = User.find(params[:id])
+        if @user.id != current_user.id 
+            redirect_to tasks_path,notice:"このページにはアクセス出来ません!"
+        end
     end
     private
     def user_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@ User.create!(
     email: 'test@test.com',
     password: 'password',
     password_confirmation: 'password'
-  ) 
+) 
