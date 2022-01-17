22329db9ed793d2d7c23d064035f7d9eb8697bc5
minorusso <gumbomb1@gmail.com>
2022-01-17 17:39:43 +0900

    modelのemailにuniquenessバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :tasks, dependent: :destroy
     validates :name,  presence: true, length: { maximum: 30 }
     before_validation { email.downcase! }
-    validates :email, presence: true, length: { maximum: 255 },
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                 format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     has_secure_password
     validates :password, length: { minimum: 6 }
