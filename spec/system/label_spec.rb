71c2a7e3abb62face77be4464e359831594f4e5a
minorusso <gumbomb1@gmail.com>
2022-01-18 13:26:03 +0900

    テスト：ラベル検索＆編集

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -14,15 +14,51 @@ RSpec.describe 'ラベル機能', type: :system do
 describe 'ラベル検索機能' do
   context 'ラベルを新規作成した場合' do
     it '作成したタスクにラベルが含まれている' do
-      visit new_task_path
-      fill_in 'task[title]', with: 'ラベル'
-      fill_in 'task[details]', with: 'ラベル検証'
-      select '高', from: 'task[priority]'
-      select '未着手', from: 'task[completed]'
-      check 'label1'
-      click_on '登録する'
-      expect(page).to have_content 'label1'
+        visit new_task_path
+        fill_in 'task[title]', with: 'ラベル'
+        fill_in 'task[details]', with: 'ラベル検証'
+        select '高', from: 'task[priority]'
+        select '未着手', from: 'task[completed]'
+        check 'label1'
+        click_on '登録する'
+        expect(page).to have_content 'label1'
     end
   end
+  context 'ラベル検索をした場合' do
+    it '検索したラベルのタスクが表示される' do
+        visit new_task_path
+        fill_in 'task[title]', with: 'ラベル'
+        fill_in 'task[details]', with: 'ラベル検証'
+        select '高', from: 'task[priority]'
+        select '未着手', from: 'task[completed]'
+        check 'label1'
+        click_on '登録する'
+        visit tasks_path
+        select'label1', from:'task[label_id]'
+        click_on 'Search' 
+        expect(page).to have_content 'label1'
+        expect(page).to have_content 'ラベル'
+    end
+  end
+  context 'ラベルを編集した場合' do
+    it '編集したラベルが表示される' do
+        visit new_task_path
+        fill_in 'task[title]', with: 'ラベル'
+        fill_in 'task[details]', with: 'ラベル検証'
+        select '高', from: 'task[priority]'
+        select '未着手', from: 'task[completed]'
+        check 'label1'
+        click_on '登録する'
+        visit tasks_path
+        visit edit_task_path(task.id)
+        check 'label2'
+        click_on '更新する'
+        expect(page).to have_content 'label1'
+        expect(page).to have_content 'label2'
+    end
+  end
+
+
+
 end
 end
